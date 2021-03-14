938dc8c51a1c51921135fe75164723c257336155
richard258 <00033218@uca.edu.sv>
2021-03-14 15:48:10 -0600

    Add: Example1

---
+++ Example1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = 0, b = 0;
+
+           Console.WriteLine("Enter a value for a:");
+           a = Int32.Parse(Console.ReadLine());
+
+           Console.WriteLine("Enter a value for b:");
+           b = Int32.Parse(Console.ReadLine());
+
+           Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a,b)}");
+        }
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
